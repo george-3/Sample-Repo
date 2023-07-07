14c2013e1a1dae8306a1cd381d5bb52c7622d424
george-3 <george.kariuki@riarauniversity.ac.ke>
2023-07-07 09:54:14 +0300

    Program that reads JSON string

--- Practice.kt
+++ Practice.kt
@@ -1,2 +1,33 @@
 package me.user.application
 
+import kotlinx.serialization.SerialName
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+
+@Serializable
+data class Person(
+    @SerialName("name")
+    val name: String,
+    @SerialName("age")
+    val age: Int,
+    @SerialName("email")
+    val email: String
+)
+
+fun parseJson(json: String): Person {
+    return Json.decodeFromString(json)
+}
+
+fun main() {
+    val json = """{
+        "name": "George Kariuki",
+        "age": 20,
+        "email": "george@example.com"
+        }"""
+
+    val person = parseJson(json)
+    println("My name is ${person.name}.")
+    println("I am ${person.age} years old.")
+    println("My email is ${person.email}.")
+}
